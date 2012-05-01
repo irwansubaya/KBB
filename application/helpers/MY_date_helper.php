03ce0a106a590e69908b2268266dcefa9a9d7fd6
Purwandi <free6300@gmail.com>
2012-05-01 14:16:33 +0700

    Update application/helpers/MY_date_helper.php

--- application/helpers/MY_date_helper.php
+++ application/helpers/MY_date_helper.php
@@ -1,8 +1,7 @@
-<?php
-	defined('BASEPATH') or exit('No direct script access');
+<?php defined('BASEPATH') or exit('No direct script access');
 
-	function convert_date($str)
-	{
-		return date('y-m-d', strtotime($str));
-	}
-	/* end of file application/helpers/MY_date_helper.php */
+function convert_date($str)
+{
+	return date('y-m-d', strtotime($str));
+}
+/* end of file application/helpers/MY_date_helper.php */
